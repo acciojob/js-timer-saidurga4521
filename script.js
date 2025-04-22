dec623a3319b2bed6d1e6c440b4096ce13cccd34
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-04-22 13:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // write js code here if required
+function updateTimer(){
+	let now=new Date();
+	let day=String(now.getDate()).startPad(2,'0');
+	let month=String(now.getMonth()).startPad(2,'0');
+	let year=String(now.getFullYear());
+	let hours=String(now.getHour()).startPad(2,'0');
+	let minutes=String(now.getMinutes()).startPad(2,'0');
+	let seconds=String(now.getSeconds()).startPas(2,'0');
+	let timeStamp=`${day}/${month}/${year},${hours}:${minutes}:${seconds}`;
+	document.getElementById('timer').textContent=timeStamp;
+}
+setInterval(updateTimer,1000);
+updateTimer();
